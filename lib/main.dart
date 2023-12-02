c2df0a5ee309fdc5334ec54832fcffd5e521bc2e
kawajiharuka <kjn.hk10001@gmail.com>
2023-12-02 13:08:33 +0900

    Storeの作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:life_counter/life_event.dart';
+import 'objectbox.g.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,7 +8,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,8 +24,25 @@ class LifeCounterPage extends StatefulWidget {
 }
 
 class _LifeCounterPageState extends State<LifeCounterPage> {
+  Store? store;
+  Box<LifeEvent>? lifeEventBox;
+  List<LifeEvent> lifeEvents = [];
+
+  Future<void> initialize() async {
+    store = await openStore();
+    lifeEventBox = store?.box<LifeEvent>();
+    lifeEvents = lifeEventBox?.getAll() ?? [];
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
